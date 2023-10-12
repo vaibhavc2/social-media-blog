d7e9908f04c60b09d9719d1584bec78d406f7440
Vaibhav Chopra <73025956+vaibhavc2@users.noreply.github.com>
2023-10-12 19:42:53 +0530

    RTE component, css structure

--- src/components/RTE.tsx
+++ src/components/RTE.tsx
@@ -1,7 +1,64 @@
-type Props = {};
+import { Editor } from "@tinymce/tinymce-react";
+import { Control, Controller, FieldValues } from "react-hook-form";
 
-const RTE = (props: Props) => {
-  return <div>RTE</div>;
+type Props = {
+  name: string;
+  control: Control<FieldValues>;
+  label: string;
+  defaultValue: string;
 };
 
-export default RTE;
+export default function RTE({
+  name,
+  control,
+  label,
+  defaultValue = ""
+}: Props) {
+  return (
+    <div className="w-full">
+      {label && <label className="mb-1 inline-block pl-1">{label}</label>}
+
+      <Controller
+        name={name || "content"}
+        control={control}
+        render={({ field: { onChange } }) => (
+          <Editor
+            initialValue={defaultValue}
+            init={{
+              initialValue: defaultValue,
+              height: 500,
+              menubar: true,
+              plugins: [
+                "image",
+                "advlist",
+                "autolink",
+                "lists",
+                "link",
+                "image",
+                "charmap",
+                "preview",
+                "anchor",
+                "searchreplace",
+                "visualblocks",
+                "code",
+                "fullscreen",
+                "insertdatetime",
+                "media",
+                "table",
+                "code",
+                "help",
+                "wordcount",
+                "anchor"
+              ],
+              toolbar:
+                "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
+              content_style:
+                "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
+            }}
+            onEditorChange={onChange}
+          />
+        )}
+      />
+    </div>
+  );
+}
